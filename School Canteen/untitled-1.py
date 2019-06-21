268a396075785175f1fef56fdb9d754fe95c4b67
WetOnionBoi <37384973+WetOnionBoi@users.noreply.github.com>
2019-06-21 12:26:50 +1200

    Add files via upload

--- School Canteen/untitled-1.py
+++ School Canteen/untitled-1.py
@@ -16,12 +16,13 @@ class Canteen:
         self.price = food_price               # to create objects that are easily referenced
         self.total = food_total
         
-    #Test Data
+    
 food =    [
           Canteen("Sushi Roll pack", "sushdiggity.jpg", 5, "$10", 0),
-          Canteen("Hot dog and Chips", "hotdiggity.jpg",  12, "$8", 0),
+          Canteen("Hot dog and Chips", "hotdiggity.jpg",  12, "$8", 0),          #Test Data
           Canteen("Ham and Cheese Sandwiches", "hamdiggity.jpg", 4, "$5", 0)
           ]
+
 
 @route("/")     #My Websites index
 @view("index")
@@ -35,42 +36,42 @@ def Canteen():
     data = dict (menu_list = food)     #Creates a dictionary for the list of food I need in my menu
     return data
 
-@route('/purchase-success/<item_id>', method = 'POST')    #This function will 
-@view('purchase-success')                                 #
-def purchase_success(item_id):
-    item_id = int(item_id)
+@route('/purchase-success/<item_id>', method = 'POST')    # This function will 
+@view('purchase-success')                                 # minus one from the stock of a purchased
+def purchase_success(item_id):                            # item and send my user to a 
+    item_id = int(item_id)                                # success page
     found_item = None   
-    for item in food: 
-        if item.id == item_id:
-            found_item  = item
-    data = dict(item = found_item)
-    found_item.stock -= 1   #minus 1 from the amount of items in stock
-    found_item.total += 1
+    for item in food:              # searches through my test data to match existing product 
+        if item.id == item_id:       # to purchased product
+            found_item  = item         
+    data = dict(item = found_item)       
+    found_item.stock -= 1   # minus 1 from the amount of items in stock
+    found_item.total += 1    # adds one if the user wanted to restock
     return data 
     
-@route('/picture/<filename>')
-def serve_picture(filename):
+@route('/picture/<filename>')                         # function exists so that I can 
+def serve_picture(filename):                            # add pictures to my website 
     return static_file(filename, root = './Images')
 
-@route("/restock")
-@view("restock")
-def restock():
+@route("/restock")                               
+@view("restock")                           # This function is used to create 
+def restock():                              # A basic restock page for people adding stock
     data = dict (menu_list = food)
     return data
 
-@route('/restock/<item_id>', method = 'POST')
+@route('/restock/<item_id>', method = 'POST')      
 @view ('restock-success')
-def restock_success(item_id):
-    item_id = int(item_id)
+def restock_success(item_id):                #This function adds one to a chosen stock and sends        
+    item_id = int(item_id)                    #users to a restock page
     found_item = None
     for item in food:
-        if item.id == item_id:
-            found_item = item
+        if item.id == item_id:         # Matches chosen item to item in food list
+            found_item = item            
     data = dict (item = found_item)
-    quantity = request.forms.get('quantity')
-    quantity = int(quantity)
-    found_item.stock += quantity
+    quantity = request.forms.get('quantity')     # gets the quantity that is wanted to add to stock from
+    quantity = int(quantity)                     # A form in my html
+    found_item.stock += quantity             # Adds quantity to stock
     
     return data
 
-run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
+run(host='0.0.0.0', port = 8080, reloader=True, debug=True)   # Creates localhost for website
